8bac2d7f346ebc829de625f246480aeebce0503d
Nicolas RAOUL <nicolas.raoul@renault.com>
2020-02-13 17:51:17 +0100

    fff

---
+++ _posts/2020-02-13-thrid-post.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Third Post
+categories:
+- General
+feature_image: "/assets/charlie-marsh-yxMnnE2eS0w-unsplash.jpg"
+---
+
+This is my very first blog post. I haven't written anything yet but I'm sure I have some great stories to tell.
--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20200213173858';
+const version = '20200213175029';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2020/02/13/thrid-post/","/general/2020/02/13/second-post/","/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
